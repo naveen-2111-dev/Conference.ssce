af2e4c9e4ed36796e5eb210effd92169864e4805
SaranSrini03 <83581210+SaranSrini03@users.noreply.github.com>
2024-10-09 11:07:58 +0530

    Commited submit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import TextComponet from '@/components'
+import HomePage from './page/HomePage'
+import SubmitForm from './page/Submit'
+import Nav from './page/Nav'
 import React from 'react'
 
 const MainPage = () => {
   return (
     <div>
-      <TextComponet/>
+      <Nav/>
+      <SubmitForm/>
     </div>
   )
 }
